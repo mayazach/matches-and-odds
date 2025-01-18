94f79c5bf73b449295ae738fc43a327fa694adea
maya <mayazaha2@gmail.com>
2025-01-18 13:39:56 +0200

    Create database with liquibase

---
+++ src/main/resources/db/changelog/sql/addData.sql
@@ -0,0 +1,150 @@
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Athens Kallithea-Volos',to_date('18/01/2025','DD/MM/YYYY'),
+     to_timestamp('12:00','HH:MI'), 'Athens Kallithea', 'Volos', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',2.3),
+    (currval('match_id_seq'),'X',3.07),
+    (currval('match_id_seq'),'2',3.2);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Levadiakos-Asteras Tripolis',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('16:30','HH:MI'), 'Levadiakos', 'Asteras Tripolis', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',2.44),
+    (currval('match_id_seq'),'X',2.99),
+    (currval('match_id_seq'),'2',3.05);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Panetolikos-OFI Crete',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('16:30','HH:MI'), 'Panetolikos', 'OFI Crete', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',2.38),
+    (currval('match_id_seq'),'X',3.14),
+    (currval('match_id_seq'),'2',3.0);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
+INSERT INTO match (description, match_date, match_time, team_a, team_b, sport)
+VALUES
+    ('Atromitos-Olympiacos Piraeus',to_date('19/01/2025','DD/MM/YYYY'),
+     to_timestamp('15:00','HH:MI'), 'Atromitos', 'Olympiacos Piraeus', 1);
+INSERT INTO match_odds (match_id, specifier, odd)
+VALUES
+    (currval('match_id_seq'),'1',6.51),
+    (currval('match_id_seq'),'X',3.88),
+    (currval('match_id_seq'),'2',1.51);
+
