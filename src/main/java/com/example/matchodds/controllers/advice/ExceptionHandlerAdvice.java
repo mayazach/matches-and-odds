66d1b5195f95d30b291856ca0221096000180c38
mzacharopoulou <mayazaha2@gmail.com>
2025-01-19 15:38:04 +0200

    Swagger added

--- src/main/java/com/example/matchodds/controllers/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/matchodds/controllers/advice/ExceptionHandlerAdvice.java
@@ -1,13 +1,15 @@
 package com.example.matchodds.controllers.advice;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
+@Hidden
 public class ExceptionHandlerAdvice {
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
